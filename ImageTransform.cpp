cc2cd2019f3879f240062b59e071e843c0abcd9c
Nader Tarek <nader.tarek.elsherbiny@gmail.com>
2021-04-12 00:09:33 +0200

    Update ImageTransform.cpp

--- ImageTransform.cpp
+++ ImageTransform.cpp
@@ -6,27 +6,9 @@
 #include "uiuc/HSLAPixel.h"
 #include "ImageTransform.h"
 
-/* ******************
-(Begin multi-line comment...)
-
-Write your name and email address in the comment space here:
-
-Name: Nader Tarek
-Email: nader.tarek.elsherbiny@gmail.com
-
-(...end multi-line comment.)
-******************** */
-
 using uiuc::PNG;
 using uiuc::HSLAPixel;
 
-/**
- * Returns an image that has been transformed to grayscale.
- *
- * The saturation of every pixel is set to 0, removing any color.
- *
- * @return The grayscale image.
- */
 PNG grayscale(PNG image) {
   /// This function is already written for you so you can see how to
   /// interact with our PNG class.
@@ -45,27 +27,6 @@ PNG grayscale(PNG image) {
 }
 
 
-
-/**
- * Returns an image with a spotlight centered at (`centerX`, `centerY`).
- *
- * A spotlight adjusts the luminance of a pixel based on the distance the pixel
- * is away from the center by decreasing the luminance by 0.5% per 1 pixel euclidean
- * distance away from the center.
- *
- * For example, a pixel 3 pixels above and 4 pixels to the right of the center
- * is a total of `sqrt((3 * 3) + (4 * 4)) = sqrt(25) = 5` pixels away and
- * its luminance is decreased by 2.5% (0.975x its original value).  At a
- * distance over 160 pixels away, the luminance will always decreased by 80%.
- * 
- * The modified PNG is then returned.
- *
- * @param image A PNG object which holds the image data to be modified.
- * @param centerX The center x coordinate of the crosshair which is to be drawn.
- * @param centerY The center y coordinate of the crosshair which is to be drawn.
- *
- * @return The image with a spotlight.
- */
 PNG createSpotlight(PNG image, int centerX, int centerY) {
 
   for (unsigned i = 0; i < image.width(); i++)
@@ -74,22 +35,7 @@ PNG createSpotlight(PNG image, int centerX, int centerY) {
     {
       HSLAPixel & pixel = image.getPixel(i, j);
       
-    //  int diffX = abs(centerX - i);
-    //  int diffY = abs(centerY - j);
-    //  int powX = pow(diffX, 2);
-    //  int powY = pow(diffY, 2);
-    //  int euclideanDist = sqrt(powX + powY);
-
-    //  if (euclideanDist > 160)
-    //  {
-    //    pixel.l = pixel.l - (80/100) * pixel.l;
-    //  }
-    //  else
-    //  {
-    //    double luminanceDec = euclideanDist * 0.005;
-    //    pixel.l =  pixel.l - (euclideanDist/100) * pixel.l;
-    //  }
-     int hor = centerX - i;
+      int hor = centerX - i;
 
       int ver = centerY - j;
 
@@ -113,17 +59,6 @@ PNG createSpotlight(PNG image, int centerX, int centerY) {
   
 }
  
-
-/**
- * Returns a image transformed to Illini colors.
- *
- * The hue of every pixel is set to the a hue value of either orange or
- * blue, based on if the pixel's hue value is closer to orange than blue.
- *
- * @param image A PNG object which holds the image data to be modified.
- *
- * @return The illinify'd image.
-**/
 PNG illinify(PNG image) {
 
   for (unsigned i = 0; i < image.width(); i++)
@@ -132,14 +67,6 @@ PNG illinify(PNG image) {
     {
       HSLAPixel & pixel = image.getPixel(i, j);
 
-      // if (abs(216 - pixel.h) > abs(11 - pixel.h))
-      // {
-      //   pixel.h = 11;
-      // }
-      // else
-      // {
-      //   pixel.h = 216;
-      // }
       if(pixel.h >= 113.5 && pixel.h <= 293.5)
       {
         pixel.h = 216;
@@ -154,20 +81,7 @@ PNG illinify(PNG image) {
 
   return image;
 }
- 
 
-/**
-* Returns an immge that has been watermarked by another image.
-*
-* The luminance of every pixel of the second image is checked, if that
-* pixel's luminance is 1 (100%), then the pixel at the same location on
-* the first image has its luminance increased by 0.2.
-*
-* @param firstImage  The first of the two PNGs, which is the base image.
-* @param secondImage The second of the two PNGs, which acts as the stencil.
-*
-* @return The watermarked image.
-*/
 PNG watermark(PNG firstImage, PNG secondImage) {
 
   for (unsigned i = 0; i < secondImage.width(); i++)
